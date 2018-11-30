a659e06c254fe35a7ebf3f8c756cedb7f6836da9
kyehyukahn <alwayswinner@erickyehyukahn.local>
2018-11-30 15:57:19 +0900

    push tx for art api

--- src/main/java/com/bsquarelab/eosj/EosApiService.java
+++ src/main/java/com/bsquarelab/eosj/EosApiService.java
@@ -1,7 +1,6 @@
 package com.bsquarelab.eosj;
 
 public interface EosApiService {
-
 	String setARTInfo(String info);
 	String setHoldersList (String holdersList);
 	String setTransactions(String Txs);
--- src/main/java/com/bsquarelab/eosj/impl/EosApiServiceImpl.java
+++ src/main/java/com/bsquarelab/eosj/impl/EosApiServiceImpl.java
@@ -1,9 +1,13 @@
 package com.bsquarelab.eosj.impl;
 
 
+import com.bsquarelab.eosj.EosApiClientFactory;
+import com.bsquarelab.eosj.EosApiRestClient;
 import com.bsquarelab.eosj.EosApiService;
 import com.bsquarelab.eosj.domain.common.WalletKeyType;
 import com.bsquarelab.eosj.domain.common.transaction.SignedPackedTransaction;
+import com.bsquarelab.eosj.domain.common.transaction.TransactionAction;
+import com.bsquarelab.eosj.domain.common.transaction.TransactionAuthorization;
 import com.bsquarelab.eosj.domain.request.chain.AbiJsonToBinRequest;
 import com.bsquarelab.eosj.domain.request.chain.RequiredKeysRequest;
 import com.bsquarelab.eosj.domain.request.chain.transaction.PushTransactionRequest;
@@ -21,38 +25,94 @@ import com.bsquarelab.eosj.domain.response.history.controlledaccounts.Controlled
 import com.bsquarelab.eosj.domain.response.history.keyaccounts.KeyAccounts;
 import com.bsquarelab.eosj.domain.response.history.transaction.Transaction;
 
-
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 public class EosApiServiceImpl implements EosApiService {
 
-    private final EosWalletApiService eosWalletApiService;
+    private String _wallet;
+    private String _wallet_password;
+    private String _account;
+    
 
-    private final EosChainApiService eosChainApiService;
-
-    private final EosHistoryApiService eosHistoryApiService;
-
-    public EosApiServiceImpl(String baseUrl){
-        eosWalletApiService = EosApiServiceGenerator.createService(EosWalletApiService.class, baseUrl);
-        eosChainApiService = EosApiServiceGenerator.createService(EosChainApiService.class, baseUrl);
-        eosHistoryApiService = EosApiServiceGenerator.createService(EosHistoryApiService.class, baseUrl);
+    public EosApiServiceImpl(String wallet, String password, String account){
+    	_wallet = wallet;
+    	_wallet_password = password;
+    	_account = account;
     }
 
-    public EosApiServiceImpl(String walletBaseUrl, String chainBaseUrl, String historyBaseUrl){
-        eosWalletApiService = EosApiServiceGenerator.createService(EosWalletApiService.class, walletBaseUrl);
-        eosChainApiService = EosApiServiceGenerator.createService(EosChainApiService.class, chainBaseUrl);
-        eosHistoryApiService = EosApiServiceGenerator.createService(EosHistoryApiService.class, historyBaseUrl);
-    }
-
+    // eosio::action void modifyart(string artHash)
     public String setARTInfo(String info) {
-		return "";
+    	return applyTx("modifyart", "artHash", info);
 	}
-	
+    
+	// void modifyown(string owHash)
 	public String setHoldersList (String holdersList) {
-		return "";
+		return applyTx("modifyown", "owHash", holdersList);
 	}
-	public String setTransactions(String Txs) {return "";}
+
+	// eosio::action void modifytx(string txHash)
+	public String setTransactions(String Txs) {
+		return applyTx("modifytx", "txHash", Txs);
+	}
+
 	public String getArtInfo() {return "";}
 	public String getHoldersList() {return "";}
 	public String getTransactions() {return "";}
+	
+	private String applyTx(String func, String param, String value) {
+        // Set the client
+        EosApiRestClient eosApiRestClient = EosApiClientFactory.newInstance("http://127.0.0.1:8899", "http://127.0.0.1:8888", "http://127.0.0.1:8888").newRestClient();
+        
+        // Open the wallet
+        eosApiRestClient.openWallet("default");
+        eosApiRestClient.unlockWallet("default", "PW5KhdSnrn1ubnUqg3xUNJ7NzjWNcaaM31Qdr2MgAmwrpj7rdLsoV");
+        
+        
+        /* Create the json array of arguments */
+        Map<String, String> args = new HashMap<String, String>(4);
+        args.put(param, value);
+        AbiJsonToBin data = eosApiRestClient.abiJsonToBin("prorata", func, args);
+
+        /* Get the head block */
+        Block block = eosApiRestClient.getBlock(eosApiRestClient.getChainInfo().getHeadBlockId());
+        String ts = block.getTimeStamp();
+
+        /* Create Transaction Action Authorization */
+        TransactionAuthorization transactionAuthorization = new TransactionAuthorization();
+        transactionAuthorization.setActor("prorata");
+        transactionAuthorization.setPermission("active");
+
+        /* Create Transaction Action */
+        TransactionAction transactionAction = new TransactionAction();
+        transactionAction.setAccount("prorata");
+        transactionAction.setName(func);
+        transactionAction.setData(data.getBinargs());
+        transactionAction.setAuthorization(Collections.singletonList(transactionAuthorization));
+        
+        /* Create a transaction */
+        PackedTransaction packedTransaction = new PackedTransaction();
+        packedTransaction.setRefBlockPrefix(block.getRefBlockPrefix().toString());
+        packedTransaction.setRefBlockNum(block.getBlockNum().toString());
+        // expired after 3 minutes
+        String expiration = ZonedDateTime.now(ZoneId.of("GMT")).plusMinutes(3).truncatedTo(ChronoUnit.SECONDS).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        packedTransaction.setExpiration(expiration);
+        packedTransaction.setRegion("0");
+        packedTransaction.setMax_net_usage_words("0");
+        packedTransaction.setActions(Collections.singletonList(transactionAction));
+
+        /* Sign the Transaction */
+        
+        SignedPackedTransaction signedPackedTransaction 
+        = eosApiRestClient.signTransaction(packedTransaction, 
+        								Collections.singletonList("EOS8f3xTQz16gM51GnmFWoE2Mwwyg7rVRq691sjbYfHTRrJYqX1cG"), 
+        								"cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f");
+        /* Push the transaction */
+        PushedTransaction pushedTransaction = eosApiRestClient.pushTransaction("none", signedPackedTransaction);  
+    	
+		return pushedTransaction.getTransactionId();	
+	}
 }
